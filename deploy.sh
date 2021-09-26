d816ba7940aaead933e14edcc745b895c01470a0
Peter Kovacs <weriKK@users.noreply.github.com>
2021-09-26 14:01:19 +0200

    Update deploy.sh
    let's not fail docker stop and rm if it's the very first deployment and the containers do not exist on the host yet

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ fi
 
 docker ps -f name=dashboard-backend    
 docker pull kovadocker/dashboard-backend:${BUILD_ID}
-docker stop dashboard-backend
-docker rm dashboard-backend
+docker stop dashboard-backend || true
+docker rm dashboard-backend || true
 docker run --name=dashboard-backend --restart=always -p 8888:8080 -d kovadocker/dashboard-backend:${BUILD_ID}
